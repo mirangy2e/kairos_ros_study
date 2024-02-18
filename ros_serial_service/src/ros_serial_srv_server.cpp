8d86b2175bd6b9050568ec53ef3e53c697ed0138
mirangy2e <76118387+mirangy2e@users.noreply.github.com>
2024-02-19 02:07:41 +0900

    Add files via upload

--- ros_serial_service/src/ros_serial_srv_server.cpp
+++ ros_serial_service/src/ros_serial_srv_server.cpp
@@ -35,7 +35,7 @@ int main(int argc, char **argv)              // Node Main Function
   ros::NodeHandle nh;                        // Node handle declaration
 
  
-  ser.setPort("/dev/ttyUSB0");  // Replace with your serial port name
+  ser.setPort("/dev/ttyACM0");  // Replace with your serial port name
   ser.setBaudrate(115200);
   serial::Timeout timeout = serial::Timeout::simpleTimeout(1000);
   ser.setTimeout(timeout);
